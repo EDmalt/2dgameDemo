70db18e8676baa0fbf35071743deb9bd4241c136
EDmalt <2283157463@qq.com>
2022-05-04 11:19:42 +0800

    2022/5/4

--- script/player_normal.gd
+++ script/player_normal.gd
@@ -10,6 +10,8 @@ var player_transform=Vector2.ZERO #人物初始运动向量
 var Time=0#时间计算-大多用于计算加速度
 var health_bars=100
 const GRAVITY=400 #自定义下落速度
+var is_jumping=false# 跳跃判定
+var move_direction=0
 
 onready var player_anim=$AnimationPlayer#获取场景树中的动画器
 onready var player=$player# 获取场景树中的人物对象
@@ -27,15 +29,34 @@ func _process(delta):
 	Time+=0.1
 	player_transform.y+=GRAVITY*delta*Time# 每帧向下移动，模仿重力
 	player_move(delta)
+	player_jump(delta)
+	animation_control()
+	print(player_transform.y)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 func player_move(delta):
 	#人物移动部分
-	var move_direction=Input.get_action_strength("player_right")-Input.get_action_strength("player_left")
+	move_direction=Input.get_action_strength("player_right")-Input.get_action_strength("player_left")
 	player_transform.x=move_toward(player_transform.x,move_direction*max_speed,(max_speed/0.2)*delta)
-	if(move_direction==0):
-		player_anim.play("player_stand")
-	else:
+
+func player_jump(delta):
+	# 跳跃
+	if Input.is_action_pressed("player_jump") and is_jumping==false:
+		player_transform.y=-GRAVITY
+		is_jumping=true
+	elif is_on_floor():
+		player_transform.y=0
+		Time=0
+		is_jumping=false
+
+func animation_control():
+	# 动画控制
+	if move_direction!=0 and is_on_floor():
 		player_anim.play("player_run")
 		$"Texture-0".flip_h=move_direction<0
+	elif GRAVITY!=-GRAVITY or not is_on_floor():
+		player_anim.play("player_jump_up")
+	if is_on_floor() and move_direction==0:
+		player_anim.play("player_stand")
